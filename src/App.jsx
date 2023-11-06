cb55ca7fd259fffed0153763168b21f0d8d90baa
FarzinKhosravi <khosravii.farzin@gmail.com>
2023-11-06 13:06:22 +0330

    setted up project using vite

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-slate-900">
+      <p className="text-2xl font-bold text-white">I LOVE REACT.JS 😎❤️</p>
+    </div>
+  );
+}
+
+export default App;
