58f5babbd9e4bd5c53083f9171de3c7a55cbd891
FarzinKhosravi <khosravii.farzin@gmail.com>
2023-11-07 17:35:28 +0330

    added layout and header components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import { Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
+
 function App() {
-  return <div className="min-h-screen bg-slate-900">App</div>;
+  return (
+    <div className="min-h-screen bg-slate-900 font-serif">
+      <Routes>
+        <Route path="/" element={<Layout />}></Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      letterSpacing: {
+        1: "1px",
+        5: "5px",
+      },
+      height: {
+        0.75: "3px",
+      },
+      rotate: {
+        129: "129deg",
+        50: "50deg",
+        360: "360deg",
+      },
+      translate: {
+        0.25: "1px",
+        2.25: "9px",
+        3.38: "13.5px",
+      },
+    },
   },
   plugins: [],
 };
