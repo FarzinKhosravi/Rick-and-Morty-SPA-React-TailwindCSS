1d5e287df6b05803bda07ecd240ac7015221631a
FarzinKhosravi <khosravii.farzin@gmail.com>
2023-11-25 14:12:06 +0330

    modified favorites and modal components

--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -4,7 +4,6 @@ import Modal from "./Modal";
 import { useFavorites } from "../context/CharacterPage/FavoritesContext";
 
 function Favorites() {
-  const favorites = useFavorites();
   const [isShowModal, setIsShowModal] = useState(false);
 
   const showModal = () => {
@@ -18,17 +17,25 @@ function Favorites() {
   return (
     <>
       <Modal onHideModal={hideModal} isShowModal={isShowModal} />
-      <div
-        onClick={showModal}
-        className="relative mr-3 cursor-pointer font-sans font-semibold md:mr-0"
-      >
-        <HeartIcon className="h-8 w-8 text-red-600" />
-        <span className="absolute -right-1 top-0 inline-flex h-4 w-4 items-center justify-center rounded-full bg-red-600 text-xs text-white">
-          <span>{favorites.length}</span>
-        </span>
-      </div>
+      <FavoritesTotal onShowModal={showModal} />
     </>
   );
 }
 
 export default Favorites;
+
+function FavoritesTotal({ onShowModal }) {
+  const favorites = useFavorites();
+
+  return (
+    <div
+      onClick={onShowModal}
+      className="relative mr-3 cursor-pointer font-sans font-semibold md:mr-0"
+    >
+      <HeartIcon className="h-8 w-8 text-red-600" />
+      <span className="absolute -right-1 top-0 inline-flex h-4 w-4 items-center justify-center rounded-full bg-red-600 text-xs text-white">
+        <span>{favorites.length}</span>
+      </span>
+    </div>
+  );
+}
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -4,8 +4,68 @@ import { useFavorites } from "../context/CharacterPage/FavoritesContext";
 import { useFavoritesDispatch } from "./../context/CharacterPage/FavoritesContext";
 
 function Modal({ isShowModal, onHideModal }) {
-  const favorites = useFavorites();
+  return (
+    <div
+      className={`fixed inset-0 z-10 h-screen items-center justify-center bg-slate-300/50 py-12 dark:bg-slate-900/50 ${
+        isShowModal ? "flex" : "hidden"
+      }`}
+    >
+      <Backdrop onHideModal={onHideModal} isShowModal={isShowModal} />
+
+      <div className="no-scrollbar h-full w-11/12 overflow-y-auto rounded-lg">
+        <div className="flex flex-col rounded-lg bg-slate-300 shadow-xl shadow-slate-900/30 dark:bg-slate-900 dark:shadow-slate-300/30">
+          <ModalHeader onHideModal={onHideModal} />
+          <FavoriteCharacters />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Modal;
+
+function Backdrop({ onHideModal, isShowModal }) {
+  return (
+    <div
+      onClick={onHideModal}
+      className={`fixed inset-0 h-screen cursor-pointer bg-slate-300/50 dark:bg-slate-900/50 ${
+        isShowModal ? "block" : "hidden"
+      }`}
+    ></div>
+  );
+}
+
+function ModalHeader({ onHideModal }) {
   const { removeAllFavoritesHandler } = useFavoritesDispatch();
+
+  return (
+    <div className="sticky left-0 right-0 top-0 z-20 bg-slate-300 p-3 dark:bg-slate-900">
+      <div className="mb-1 flex items-center justify-between pb-1">
+        <h2 className="text-lg font-semibold text-slate-900 dark:text-slate-300">
+          List of Favorites
+        </h2>
+        <div className="flex items-center justify-end">
+          <div className="mr-2">
+            <FireIcon
+              onClick={removeAllFavoritesHandler}
+              className="h-6 w-6 cursor-pointer text-yellow-500"
+            />
+          </div>
+          <div>
+            <XCircleIcon
+              onClick={onHideModal}
+              className="h-6 w-6 cursor-pointer text-red-600"
+            />
+          </div>
+        </div>
+      </div>
+      <hr className="mb-6 h-px border-0 bg-slate-600" />
+    </div>
+  );
+}
+
+function FavoriteCharacters() {
+  const favorites = useFavorites();
   const { removeFavoriteCharacter } = useFavoritesDispatch();
 
   function renderFavoriteCharacters() {
@@ -22,49 +82,8 @@ function Modal({ isShowModal, onHideModal }) {
   }
 
   return (
-    <div
-      className={`fixed inset-0 z-10 h-screen items-center justify-center bg-slate-300/50 py-12 dark:bg-slate-900/50 ${
-        isShowModal ? "flex" : "hidden"
-      }`}
-    >
-      <div
-        onClick={onHideModal}
-        className={`fixed inset-0 h-screen cursor-pointer bg-slate-300/50 dark:bg-slate-900/50 ${
-          isShowModal ? "block" : "hidden"
-        }`}
-      ></div>
-
-      <div className="no-scrollbar h-full w-11/12 overflow-y-auto rounded-lg">
-        <div className="flex flex-col rounded-lg bg-slate-300 shadow-xl shadow-slate-900/30 dark:bg-slate-900 dark:shadow-slate-300/30">
-          <div className="sticky left-0 right-0 top-0 z-20 bg-slate-300 p-3 dark:bg-slate-900">
-            <div className="mb-1 flex items-center justify-between pb-1">
-              <h2 className="text-lg font-semibold text-slate-900 dark:text-slate-300">
-                List of Favorites
-              </h2>
-              <div className="flex items-center justify-end">
-                <div className="mr-2">
-                  <FireIcon
-                    onClick={removeAllFavoritesHandler}
-                    className="h-6 w-6 cursor-pointer text-yellow-500"
-                  />
-                </div>
-                <div>
-                  <XCircleIcon
-                    onClick={onHideModal}
-                    className="h-6 w-6 cursor-pointer text-red-600"
-                  />
-                </div>
-              </div>
-            </div>
-            <hr className="mb-6 h-px border-0 bg-slate-600" />
-          </div>
-          <div className="z-10 flex flex-col gap-y-4 px-3 pb-3">
-            {renderFavoriteCharacters()}
-          </div>
-        </div>
-      </div>
+    <div className="z-10 flex flex-col gap-y-4 px-3 pb-3">
+      {renderFavoriteCharacters()}
     </div>
   );
 }
-
-export default Modal;
