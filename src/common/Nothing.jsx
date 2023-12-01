2471edd31cea5f5aee1b07cf55e08187107f045c
FarzinKhosravi <khosravii.farzin@gmail.com>
2023-12-01 18:16:41 +0330

    modified nothing component

--- src/common/Nothing.jsx
+++ src/common/Nothing.jsx
@@ -1,3 +1,5 @@
+import notFound from "../assets/images/notFound.png";
+
 function Nothing() {
   return (
     <div className="flex flex-col items-center justify-center">
@@ -7,11 +9,7 @@ function Nothing() {
         </span>
       </div>
       <div className="max-w-72">
-        <img
-          className="block w-full"
-          src="../../../public/00.png"
-          alt="Not Found"
-        />
+        <img className="block w-full" src={notFound} alt="Not Found" />
       </div>
       <div className="w-20 -translate-x-16 -translate-y-8 -rotate-45">
         <span className="block text-2xl font-black text-slate-800 dark:text-yellow-400">
