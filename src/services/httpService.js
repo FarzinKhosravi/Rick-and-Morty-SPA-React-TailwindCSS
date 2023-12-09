0d89cf0fea9e13dea1e3f23c1e61267cf284b308
FarzinKhosravi <khosravii.farzin@gmail.com>
2023-12-09 18:04:15 +0330

    JSON Server went online

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:3000";
+axios.defaults.baseURL = "https://rick-and-morty-database.onrender.com/";
 
 const http = {
   get: axios.get,
